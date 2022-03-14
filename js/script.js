e3f56c3435fdf172c885224840635aecef9412fc
jidraph kimachia <jidraphkimachia1@gmail.com>
2022-03-14 14:15:32 +0300

    added hover

--- js/script.js
+++ js/script.js
@@ -22,68 +22,7 @@ $(document).ready(function(){
     });
 });
 
-// portfolio
-$(document).ready(function(){
-    $("#work8").hover(function(){
-      $("#8work").show();
-    }).hover(function(){
-      $("#8work").hide();
-    });
-  });
 
-  $(document).ready(function(){
-    $("#work7").hover(function(){
-      $("#7work").show();
-    }).hover(function(){
-      $("#7work").hide();
-    });
-  });
 
-  $(document).ready(function(){
-    $("#work6").hover(function(){
-      $("#6work").show();
-    }).hover(function(){
-      $("#6work").hide();
-    });
-  });
 
-  $(document).ready(function(){
-    $("#work5").hover(function(){
-      $("#5work").show();
-    }).hover(function(){
-      $("#5work").hide();
-    });
-  });
-
-  $(document).ready(function(){
-    $("#work4").hover(function(){
-      $("#4work").show();
-    }).hover(function(){
-      $("#4work").hide();
-    });
-  });
-
-  $(document).ready(function(){
-    $("#work3").hover(function(){
-      $("#3work").show();
-    }).hover(function(){
-      $("#3work").hide();
-    });
-  });
-
-  $(document).ready(function(){
-    $("#work2").hover(function(){
-      $("#2work").show();
-    }).hover(function(){
-      $("#2work").hide();
-    });
-  });
-
-  $(document).ready(function(){
-    $("#work1").hover(function(){
-      $("#1work").show();
-    }).hover(function(){
-      $("#1work").hide();
-    });
-  });
 
