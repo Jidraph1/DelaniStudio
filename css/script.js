f2beb2ee02a5c4f49f1076ef1848d74b3023e5f3
jidraph kimachia <jidraphkimachia1@gmail.com>
2022-03-14 09:04:41 +0300

    js

--- css/script.js
+++ css/script.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function(){
     $("#design").click(function() {
-        $("#designImg").toggle();
-        $("#designText").toggle();
+        $("#designText").slideToggle();
+        $("#designImg").slideToggle();
     });
 });
 
-
+// portfolio?
 $(document).ready(function(){
     $("#work8").hover(function(){
       $("#8work").show();
@@ -15,4 +15,59 @@ $(document).ready(function(){
     });
   });
 
-$
+  $(document).ready(function(){
+    $("#work7").hover(function(){
+      $("#7work").show();
+    }).hover(function(){
+      $("#7work").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#work6").hover(function(){
+      $("#6work").show();
+    }).hover(function(){
+      $("#6work").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#work5").hover(function(){
+      $("#5work").show();
+    }).hover(function(){
+      $("#5work").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#work4").hover(function(){
+      $("#4work").show();
+    }).hover(function(){
+      $("#4work").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#work3").hover(function(){
+      $("#3work").show();
+    }).hover(function(){
+      $("#3work").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#work2").hover(function(){
+      $("#2work").show();
+    }).hover(function(){
+      $("#2work").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#work1").hover(function(){
+      $("#1work").show();
+    }).hover(function(){
+      $("#1work").hide();
+    });
+  });
+
